03f752c02ce2d025335384aa1a79748ca15faf9c
Nicolas Fella <nicolas.fella@gmx.de>
2020-08-17 13:07:30 +0200

    Add kotlin file extension
    We have kotlin files in kdeconnect-android

--- directoryparser.cpp
+++ directoryparser.cpp
@@ -10,7 +10,7 @@
 #include <QDebug>
 #include <QVector>
 
-const QStringList DirectoryParser::s_supportedExtensions = { ".cpp", ".cc", ".c", ".h", ".css", ".hpp", ".qml", ".cmake", "CMakeLists.txt", ".in", ".py", ".frag", ".vert", ".glsl", "php", "sh", ".mm", ".java" };
+const QStringList DirectoryParser::s_supportedExtensions = { ".cpp", ".cc", ".c", ".h", ".css", ".hpp", ".qml", ".cmake", "CMakeLists.txt", ".in", ".py", ".frag", ".vert", ".glsl", "php", "sh", ".mm", ".java", ".kt" };
 
 QRegularExpression DirectoryParser::spdxRegExp() const
 {
