8ef3bd015dfd5d0b53af5391f3529907710b9196
David Redondo <kde@david-redondo.de>
2020-09-10 11:46:59 +0200

    Add MIT license as found in various breeze files
    Also account for license headers starting with varying numbers of //

--- licenseregistry.cpp
+++ licenseregistry.cpp
@@ -115,10 +115,10 @@ QRegularExpression LicenseRegistry::headerTextRegExp(const SpdxExpression &ident
     for (const QString &header : m_registry.value(identifier)) {
         QString pattern(QRegularExpression::escape(header));
         // start detection at first word of license string to make detection easier
-        pattern.replace("\\\n", "[#\\* ]*\\\n[#\\* ]*"); // allow prefixes and suffices of whitespace mixed with stars
+        pattern.replace("\\\n", "[#\\* \\/]*\\\n[#\\* \\/]*"); // allow prefixes and suffices of whitespace mixed with stars
 
         // remove line-break pattern from last line
-        pattern = pattern.left(pattern.length() - QString("[\\* ]*\\\n[\\* ]*").length());
+        pattern = pattern.left(pattern.length() - QString("[\\* \\/]*\\\n[\\* \\/]*").length());
         pattern.append("[\\* ]*");
 
         patterns.append(pattern);
