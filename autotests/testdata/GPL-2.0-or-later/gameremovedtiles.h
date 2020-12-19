4ee51cc908eb7cd1e6d82a2a556ca1fb0a265673
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-12-19 16:43:32 +0100

    Add license headers found in kmahjongg
    The actual text was "KMahjongg is free software", but I replaced
    "KMahjongg" manually with "This program" first, to assist licensedigger
    a bit.

---
+++ autotests/testdata/GPL-2.0-or-later/gameremovedtiles.h
@@ -0,0 +1,138 @@
+/* Copyright (C) 2019 Christian Krippendorf <Coding@Christian-Krippendorf.de>
+ *
+ * This program is free software; you can redistribute it and/or modify it under 
+ * the terms of the GNU General Public License as published by the Free 
+ * Software Foundation; either version 2 of the License, or (at your option) 
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT 
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more 
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, write to the Free Software Foundation, Inc., 51 
+ * Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA. */
+
+#ifndef GAMEREMOVEDTILES_H
+#define GAMEREMOVEDTILES_H
+
+// Qt
+#include <QGraphicsObject>
+#include <QList>
+
+// KMahjongg
+#include "kmtypes.h"
+
+// Forward declarations
+class KMahjonggTileset;
+class GameData;
+
+
+/**
+ * A QGraphicsObject for representing the removed tiles of the current game.
+ * @author Christian Krippendorf 
+ */
+class GameRemovedTiles : public QGraphicsObject
+{
+    Q_OBJECT
+
+public:
+    /**
+     * Constructor
+     * @param item The parent item
+     */
+    explicit GameRemovedTiles(QGraphicsObject *item = nullptr);
+    ~GameRemovedTiles() override;
+
+    /**
+     * Overrides the paint method of QGraphicsItem
+     */
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option,
+            QWidget *widget) override;
+
+    /**
+     * Set size of element
+     * @param width Width of element in pixels
+     * @param height Height of element in pixels
+     */
+    void setSize(qreal width, qreal height);
+    
+    /**
+     * Overrides the boundingRect method of QGraphicsItem
+     */
+    QRectF boundingRect() const override;
+
+    /**
+     * Returns the rect of the item
+     * @return The rect of the item
+     */
+    QRectF rect() const;
+
+    /**
+     * Called in GameView::resizeTileset() before reloading the tiles
+     */
+    void prepareForGeometryChange();
+
+    /**
+     * Add an removed tile.
+     * @param itemPos POSITION object of the item.
+     */
+    void addItem(const POSITION & itemPos);
+
+    /**
+     * Set the game data object.
+     * @param gameData the game data object.
+     */
+    void setGameData(GameData * gameData);
+
+    /**
+     * Remove a tile.
+     */
+    void removeLastItem();
+
+    /**
+     * Set the tileset for the tile pixmaps.
+     * @param tiles KMahjonggTileset object.
+     */
+    void setTileset(KMahjonggTileset * tiles);
+
+    /**
+     * Calculate all values that necessary to paint all tiles.
+     */
+    void updateTileCalculations();
+
+    /**
+     * Reset system for empty game.
+     */
+    void reset();
+
+    /**
+     * Remove the last two added tiles.
+     */
+    void undo();
+
+private:
+    qreal m_width;
+    qreal m_height;
+    qreal m_borderWidthFrac;
+    qreal m_tileScale;
+    qreal m_titleHeightFrac;
+    qreal m_borderWidthPixel;
+    qreal m_titleHeightPixel;
+    qreal m_tileSpaceRow;
+    qreal m_tileSpaceCol;
+    qreal m_tileFaceWidth;
+    qreal m_tileFaceHeight;
+    qreal m_faceScale;
+    qreal m_tileFaceWidthScaled;
+    qreal m_tileFaceHeightScaled;
+    unsigned int m_maxTilesRow;
+    unsigned int m_maxTilesCol;
+
+    QList<USHORT> * m_itemFaces;
+    KMahjonggTileset * m_tiles;
+    GameData * m_gameData;
+};
+
+#endif // GAMEREMOVEDTILES_H
---
+++ autotests/testdata/GPL-2.0-or-later/kmahjongg.h
@@ -0,0 +1,123 @@
+/* kmahjongg, the classic mahjongg game for KDE project
+ *
+ * Copyright (C) 1997 Mathias Mueller   <in5y158@public.uni-hamburg.de>
+ * Copyright (C) 2006-2007 Mauricio Piacentini   <mauricio@tabuleiro.com>
+ *
+ * This program is free software; you can redistribute it and/or modify it under the terms of the GNU
+ * General Public License as published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program; if
+ * not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA. */
+
+#ifndef KMAHJONGG_H
+#define KMAHJONGG_H
+
+// KF
+#include <KXmlGuiWindow>
+
+class KToggleAction;
+class QLabel;
+class KGameClock;
+class Editor;
+class GameView;
+class GameScene;
+class GameData;
+class KMahjonggLayout;
+
+/**
+ * @author Mathias */
+class KMahjongg : public KXmlGuiWindow
+{
+    Q_OBJECT
+
+public:
+    explicit KMahjongg(QWidget * parent = nullptr);
+    ~KMahjongg() override;
+
+public Q_SLOTS:
+    void startNewGame(int num = -1);
+
+    /**
+     * Load the settings... */
+    void loadSettings();
+
+    void showStatusText(const QString & msg, long board);
+
+    void showItemNumber(int maximum, int current, int left);
+
+    void gameOver(unsigned short numRemoved, unsigned short cheats);
+
+    /**
+     * Connected to GameView::demoOrMoveListAnimationOver(bool) signal. */
+    void demoOrMoveListAnimationOver(bool demoGameLost);
+
+protected:
+    void setupKAction();
+    void setupStatusBar();
+    void changeEvent(QEvent * event) override;
+    void closeEvent(QCloseEvent * event) override;
+
+private Q_SLOTS:
+    void showSettings();
+    void startNewNumeric();
+    void saveGame();
+    void loadGame();
+    void restartGame();
+    void undo();
+    void redo();
+    void pause();
+    void demoMode();
+    void displayTime(const QString & timestring);
+    void showHighscores();
+    void slotBoardEditor();
+    void noMovesAvailable();
+    void toggleFullscreen(bool fullscreen);
+
+private:
+    enum class GameState { Gameplay,
+                           Demo,
+                           Paused,
+                           Finished,
+                           Stuck };
+    GameState m_gameState;
+
+    void updateState(GameState state);
+    void updateUndoAndRedoStates();
+    void loadLayout();
+    void saveSettings();
+
+    bool m_bLastRandomSetting;
+
+    GameView * m_gameView;
+    GameData * m_gameData;
+    GameScene * m_gameScene;
+
+    KMahjonggLayout * m_boardLayout;
+
+    Editor * m_boardEditor;
+
+    QLabel * m_gameNumLabel;
+    QLabel * m_tilesLeftLabel;
+    QLabel * m_statusLabel;
+    QLabel * m_gameTimerLabel;
+
+    QAction * m_undoAction;
+    QAction * m_redoAction;
+
+    KGameClock * m_gameTimer;
+
+    KToggleAction * m_pauseAction;
+    KToggleAction * m_fullscreenAction;
+    KToggleAction * m_demoAction;
+
+    static const QString gameMagic;
+    static const int gameDataVersion;
+};
+
+#endif
