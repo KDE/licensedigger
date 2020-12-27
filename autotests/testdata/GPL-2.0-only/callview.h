750f6be934df6dc5beb7fefe5944d50fbcc8f6a2
Friedrich W. H. Kossebau <kossebau@kde.org>
2020-12-27 06:38:29 +0100

    Add license headers found in kcachegrind
    The actual text of the GPL-2.0-only licensed files had "KCachegrind"
    instead of "This program" in the text, but I replaced that manually first,
    to assist licensedigger a bit.

---
+++ autotests/testdata/GPL-2.0-only/callview.h
@@ -0,0 +1,60 @@
+/* This file is part of KCachegrind.
+   Copyright (c) 2003-2016 Josef Weidendorfer <Josef.Weidendorfer@gmx.de>
+
+   This program is free software; you can redistribute it and/or
+   modify it under the terms of the GNU General Public
+   License as published by the Free Software Foundation, version 2.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program; see the file COPYING.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+/*
+ * Call Views
+ */
+
+#ifndef CALLVIEW_H
+#define CALLVIEW_H
+
+#include <QTreeWidget>
+#include "tracedata.h"
+#include "traceitemview.h"
+
+class CallView: public QTreeWidget, public TraceItemView
+{
+    Q_OBJECT
+
+public:
+    CallView(bool showCallers, TraceItemView* parentView,
+             QWidget* parent=nullptr);
+
+    QWidget* widget() override { return this; }
+    QString whatsThis() const override;
+    bool showCallers() const { return _showCallers; }
+
+protected Q_SLOTS:
+    void context(const QPoint &);
+    void selectedSlot(QTreeWidgetItem*, QTreeWidgetItem*);
+    void activatedSlot(QTreeWidgetItem*, int);
+    void headerClicked(int);
+
+protected:
+    void keyPressEvent(QKeyEvent* event) override;
+
+private:
+    CostItem* canShow(CostItem*) override;
+    void doUpdate(int, bool) override;
+    void refresh();
+    void setCostColumnWidths();
+
+    bool _showCallers;
+};
+
+#endif
